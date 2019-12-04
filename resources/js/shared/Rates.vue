1e941530ff0c32be3a8722a9bb7fb0faa016723c
Clayton D'Mello <clayton@chargefield.com>
2019-12-04 17:34:17 -0500

    Add animation

--- resources/js/shared/Rates.vue
+++ resources/js/shared/Rates.vue
@@ -1,12 +1,14 @@
 <template>
-  <div v-if="hasRates">
-    <p class="text-center uppercase font-bold text-sm">Daily Rates</p>
-    <div
-      class="flex flex-col sm:flex-row justify-center items-center flex-wrap bg-white rounded-lg p-4 shadow-inner uppercase mt-4"
-    >
-      <div class="px-4 py-2" v-for="rate in rates" :key="rate.duration">{{ rate.label }}</div>
+  <transition name="fade">
+    <div v-if="hasRates">
+      <p class="text-center uppercase font-bold text-sm">Daily Rates</p>
+      <div
+        class="flex flex-col sm:flex-row justify-center items-center flex-wrap bg-white rounded-lg p-4 shadow-inner uppercase mt-4"
+      >
+        <div class="px-4 py-2" v-for="rate in rates" :key="rate.duration">{{ rate.label }}</div>
+      </div>
     </div>
-  </div>
+  </transition>
 </template>
 
 <script>
