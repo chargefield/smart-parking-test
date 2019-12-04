90f7998da4382ac48d3de047c12e22e576bb3999
Clayton D'Mello <clayton@chargefield.com>
2019-12-04 14:37:25 -0500

    Add a small attribute

--- resources/js/shared/Logo.vue
+++ resources/js/shared/Logo.vue
@@ -1,11 +1,15 @@
 <template>
-  <div class="flex justify-center items-baseline text-3xl italic">
+  <div class="flex justify-center items-baseline italic" :class="small ? 'text-xl' : 'text-3xl'">
     <span>smart</span>
-    <span class="text-5xl">PARK</span>
-    <span class="text-4xl">ing</span>
+    <span :class="small ? 'text-3xl' : 'text-5xl'">PARK</span>
+    <span :class="small ? 'text-2xl' : 'text-4xl'">ing</span>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  props: {
+    small: { type: Boolean, default: false }
+  }
+}
 </script>
