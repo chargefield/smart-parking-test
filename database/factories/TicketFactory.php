8acbe3acf9028db0100ec9953ceb9f27c59a9f91
Clayton D'Mello <clayton@chargefield.com>
2019-12-04 02:25:48 -0500

    Add ticket table, model and factory

---
+++ database/factories/TicketFactory.php
@@ -0,0 +1,18 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Ticket;
+use Faker\Generator as Faker;
+
+$factory->define(Ticket::class, function (Faker $faker) {
+    return [
+        'paid_at' => null,
+    ];
+});
+
+$factory->state(Ticket::class, 'paid', function (Faker $faker) {
+    return [
+        'paid_at' => now(),
+    ];
+});
