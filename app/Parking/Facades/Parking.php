fece2170ee4b31733b3cde957e726686914c0845
Clayton D'Mello <clayton@chargefield.com>
2019-12-04 23:52:30 -0500

    Update Parking.php

--- app/Parking/Facades/Parking.php
+++ app/Parking/Facades/Parking.php
@@ -6,8 +6,17 @@ use App\Parking\Manager;
 use Illuminate\Support\Facades\Facade;
 
 /**
+ * @method static \Illuminate\Support\Collection findAllUnpaidTickets()
+ * @method static \Illuminate\Support\Collection findAllPaidTickets()
+ * @method static \App\Ticket|null findTicket(string $hash, bool $all = false)
+ * @method static \App\Ticket|null createTicket()
+ * @method static \Illuminate\Support\Collection getUsedTickets()
+ * @method static bool hasAvailableSpace()
+ * @method static int spacesAvailable()
  * @method static int getTotalSpaces()
  * @method static \App\Parking\Manager setTotalSpaces(int $spaces)
+ * @method static int getTicketExpiredDelay()
+ * @method static \App\Parking\Manager setTicketExpiredDelay(int $minutes)
  *
  * @see \App\Parking\Manager
  */
